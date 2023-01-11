7ee79a1a7c5a10ea8fe8e37c4eff545348bbd262
Harsh Verma <120256045+ISROHarsh@users.noreply.github.com>
2023-01-11 07:32:22 +0530

    Update main.py

--- main.py
+++ main.py
@@ -21,17 +21,21 @@ def banner():
 
 def products():
     products = PrettyTable(["Product No.","Product Name","Price per Kg"])
-    products.add_row(["1","Gulab Jamun","₹500"])
-    products.add_row(["2","Rasmalai","₹600"])
-    products.add_row(["3","Motichoor Ladoo","₹400"])
-    products.add_row(["4","Boondi","₹200"])
-    products.add_row(["5","Rasgulla","₹300"])
-    products.add_row(["6","Kaju Katli","₹1000"])
-    products.add_row(["7","Jalebi","₹350"])
-    products.add_row(["8","Bareli ki Barfi","₹500"])
-    products.add_row(["9","SoanPapdi","₹600"])
-    products.add_row(["10","Agra ka Petha","₹250"])
+    products.add_row(["1","Gulab Jamun",f"₹{500}"])
+    products.add_row(["2","Rasmalai",f"₹{600}"])
+    products.add_row(["3","Motichoor Ladoo",f"₹{400}"])
+    products.add_row(["4","Boondi",f"₹{200}"])
+    products.add_row(["5","Rasgulla",f"₹{300}"])
+    products.add_row(["6","Kaju Katli",f"₹{1000}"])
+    products.add_row(["7","Jalebi",f"₹{350}"])
+    products.add_row(["8","Bareli ki Barfi",f"₹{500}"])
+    products.add_row(["9","SoanPapdi",f"₹{600}"])
+    products.add_row(["10","Agra ka Petha",f"₹{250}"])
     return print(products)
+
+sr_no = ["1","2","3","4","5","6","7","8","9","10"]
+prod_name = ["Gulab Jamun","Rasmalai","Motichoor Ladoo","Boondi","Rasgulla","Kaju Katli","Jalebi","Bareli ki Barfi","SoanPapdi","Agra ka Petha"]
+price_kg = [500,600,400,200,300,1000,350,500,600,250]
 
 def main_menu():
     print('''\n\n
@@ -46,15 +50,36 @@ def customer_info():
     Adress = input("Enter Address: >")
     if len(mobile_no) < 10 or len(mobile_no) > 10:
         print("[\x1b[31m!\x1b[0m] The Phone no. you have entered is not of 10 digits , Please Try Again.")
+        customer_info()
+
 def purchase():
-    print("If you want to see the prducts again type Y if not then type N to go for the purchase")
-    choose = input("Enter Your choice \x1b[36m>\x1b[0m ")
-    if choose == "Y" or "y":
-        products()
-        print("")
-
-
-
+    while True:
+        try:
+            product_no = input("Enter the product no. : ")
+            if not product_no:
+                print(" Please enter a valid choice")
+            quan = float(input("Enter Qauntity in Kg : "))
+            price = quan * price_kg[int(product_no)]
+            i = 0
+            bag = input(f' Carry bag ₹10 (Y/N)> ')
+            if bag not in ["Y", "N", "YES", "NO"]:
+                print(" Please enter a valid choice (Y/N)")
+                input("Press ANY KEY to continue" )
+                banner()
+                # invoice()
+                if bag in ["Y","YES","y","yes"]:
+                    i += price 
+                    total = i + 10
+                else:
+                    i += price
+        except KeyboardInterrupt:
+            banner()
+            main_menu()
+        except Exception as e:
+            print(f"[\x1b[31m!\x1b[0m] {e.args}")
+            input(f'\u001b[0m[\x1b[\x1b[38;5;63m#\u001b[0m] Press ANY KEY to continue')
+            banner()
+            purchase()
 
 
 banner()
@@ -66,5 +91,6 @@ while True:
         products()
     if choice == 2:
         customer_info()
+        purchase()
     if choice == 3:
         quit()
