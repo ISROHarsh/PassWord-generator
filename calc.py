da6c8725bfd0d249cd80407546a481827a2cad31
Harsh Verma <120256045+ISROHarsh@users.noreply.github.com>
2023-01-11 17:52:59 +0530

    Update calc.py

--- calc.py
+++ calc.py
@@ -9,30 +9,30 @@ print("choose 5 For floor division")
 print("choose 6 For exponent")
 print("choose 7 To quit the program")
 while True:
-    choice = int(input("Enter your choice: "))
+    choice = int(input("enter your choice: "))
     if choice == 1:
-        a = int(input("Enter first number: "))
-        b = int(input("Enter second number: "))
+        a = int(input("enter first number: "))
+        b = int(input("enter second number: "))
         print("Result:", a + b)
     elif choice == 2:
-        c = int(input("Enter first number: "))
-        d = int(input("Enter second number: "))
+        c = int(input("enter first number: "))
+        d = int(input("enter second number: "))
         print("Result:", c - d)
     elif choice == 3:
-        e = input("Enter first number: ")
-        f = input("Enter second number: ")
+        e = input("enter first number: ")
+        f = input("enter second number: ")
         print("Result:", e * f)
     elif choice == 4:
-        g = int(input("Enter divident: "))
-        h = int(input("Enter divisor: "))
+        g = int(input("enter divident: "))
+        h = int(input("enter divisor: "))
         print("Quotient:", g / h)
     elif choice == 5:
-        i = int(input("Enter dividend: "))
-        j = int(input("Enter divisor: "))
+        i = int(input("enter dividend: "))
+        j = int(input("enter divisor: "))
         print("Result:", i // j)
     elif choice == 6:
-        k = int(input("\nEnter base value: "))
-        l = int(input("Enter exponential power: "))
+        k = int(input("enter base value: "))
+        l = int(input("enter exponential power: "))
         print("Result:", k ** l) 
     elif choice == 7:
         print(" Thanks for using the calculator ")
